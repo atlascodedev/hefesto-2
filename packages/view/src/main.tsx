2dc71cf954b9bab4cb584d769a4fd24676da2d29
oparin10 <alex.xande10@gmail.com>
2021-05-12 18:01:27 -0300

    [Port] Initialized process of porting CRA app to use vite with ESBUILD for faster builds and better development experience, added main entry point and got it to build with success

--- packages/view/src/main.tsx
+++ packages/view/src/main.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux";
+import CssBaseline from "@material-ui/core/CssBaseline/index";
+import { MuiThemeProvider } from "@material-ui/core";
+import { theme, styledTheme } from "./theme";
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  <React.Fragment>
+    <Provider store={store}>
+      <CssBaseline />
+      <MuiThemeProvider theme={theme}>
+        <StyledThemeProvider theme={styledTheme}>
+          <App />
+        </StyledThemeProvider>
+      </MuiThemeProvider>
+    </Provider>
+  </React.Fragment>,
+  document.getElementById("root")
+);
