954f3526eb6dd57bb9b9e3af2f215da8af93c5ef
oparin10 <alex.xande10@gmail.com>
2021-05-13 19:45:53 -0300

    [Feature] Further integrated rollup with babel and typescript to create a build process to properly generate UMD output, typings, ES modules and CommonJS modules

--- packages/library/rollup.config.js
+++ packages/library/rollup.config.js
@@ -1,35 +1,47 @@
 import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import * as path from "path";
+import { terser } from "rollup-plugin-terser";
+import babel from "@rollup/plugin-babel";
 
 export default [
   {
     input: path.resolve(__dirname, "src", "index.ts"),
     output: {
+      file: `dist/index.min.js`,
+      format: "umd",
+      name: "library",
+      esModule: false,
+      exports: "named",
       sourcemap: true,
-      dir: "dist/cjs",
     },
     plugins: [
       resolve(),
-      typescript({
-        declarationDir: "dist/cjs",
-        module: "commonjs",
-      }),
+      typescript({ declaration: false }),
+      babel({ babelHelpers: "bundled" }),
+      terser(),
     ],
   },
 
   {
     input: path.resolve(__dirname, "src", "index.ts"),
     output: {
+      file: "dist/index.esm.js",
+      format: "esm",
+      exports: "named",
       sourcemap: true,
-      dir: "dist/esm",
     },
-    plugins: [
-      resolve(),
-      typescript({
-        declarationDir: "dist/esm",
-        module: "ESNEXT",
-      }),
-    ],
+    plugins: [resolve(), typescript({ declaration: false })],
+  },
+
+  {
+    input: path.resolve(__dirname, "src", "index.ts"),
+    output: {
+      file: "dist/index.js",
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+    },
+    plugins: [resolve(), typescript({ declaration: false })],
   },
 ];
